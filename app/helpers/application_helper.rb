735742fb054030d1509dbf95f5904cf0cea92b37
unknown <anton.baranoff@gmail.com>
2012-03-08 15:20:20 +0100

    hw2 part 3b

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 module ApplicationHelper
   
-  def sortable(column, title = nil)
+  def th_sortable(column, title = nil)
     title ||= column.titleize
     current = column == sort_column
     css_class = current ? "current #{sort_direction}" : nil
-    id_name = current ? "#{column}_header" : nil
+    id_name = "#{column}_header"
     direction = current && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class, :id => id_name}
+    haml_tag :th, :class => css_class do
+      link_to title, {:sort => column, :direction => direction}, {:id => id_name}
+    end
   end
 
 end
