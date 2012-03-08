8bf06aea638470a915bfedf9611be6dd90c079d2
unknown <anton.baranoff@gmail.com>
2012-03-08 14:15:12 +0100

    hw part 3b

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,9 @@ module ApplicationHelper
   def sortable(column_names, column, title = nil)
     title ||= column.titleize
     css_class = column == sort_column(column) ? "current #{sort_direction}" : nil
+    id_name = column == sort_column(column) ? "#{column}_header" : nil
     direction = column == sort_column(column) && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class, :id => id_name}
   end
   
   def sort_column(column_names, column=nil)
