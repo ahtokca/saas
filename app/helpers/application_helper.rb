b7962e26b27a6d87763901a8a2e12ceb3d18c020
unknown <anton.baranoff@gmail.com>
2012-03-08 15:22:42 +0100

    hw2 part 3b

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     css_class = current ? "current #{sort_direction}" : nil
     id_name = "#{column}_header"
     direction = current && sort_direction == "asc" ? "desc" : "asc"
-    haml_tag :th, :class => css_class do
+    capture_haml :th, :class => css_class do
       link_to title, {:sort => column, :direction => direction}, {:id => id_name}
     end
   end
